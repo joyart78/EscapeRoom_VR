d8fdbde8e9bcac275de9e9a55367cea2ecb9138e
joyart78 <peta04033@gmail.com>
2024-12-18 12:56:41 +0500

    fix

---
+++ Assets/Password.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+using Valve.VR;
+
+public class Password : MonoBehaviour
+{
+    public Text result;
+    private string sumString;
+    private int sum = 0;
+    private int count = 0;
+    private int answer = 2538;
+    public GameObject Player;
+
+    public Transform endingTransform;
+
+    public AudioSource bgmAudioSource;
+    public AudioClip endingClip; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        result.text = "";
+    }
+
+
+    // Update is called once per frame
+    public void OnButtonDown(string numTag)
+    {
+        GetButtonNumber(numTag);
+        
+    }
+
+    public void GetButtonNumber(string numTag)
+    {
+        if (int.Parse(numTag) == -1)
+        {
+            result.text = "";
+            sum = 0;
+            count = 0;
+            sumString = "";
+        }
+        else
+        {
+
+            result.text = result.text + numTag + " ";
+            sumString += numTag;
+
+            count += 1;
+            if (count >= 4)
+            {
+                count = 0;
+                sum = int.Parse(sumString);
+
+                if (answer == sum)
+                {
+                    Player.transform.position = endingTransform.position;
+                    Player.transform.rotation = endingTransform.rotation;
+
+                    bgmAudioSource.clip = endingClip;
+                    bgmAudioSource.Play();
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# EscapeRoom-VR-Game
+ It is not optimized because it is a project that is applied only to the basics of Unity and C#.
+ 
+ 
+# Screenshot
+![](./images/example1.png)
+
+![](./images/example2.png)
+
+![](./images/example3.png)
+
+![](./images/example4.png)
+
+![](./images/example5.png)
+ 
